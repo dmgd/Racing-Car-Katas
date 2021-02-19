ad686e61f53a2fcaf3a9b5381cbe89648f1a0f7e
jordan <jordan.r.stewart@gmail.com>
2021-02-19 17:12:11 +0000

    .

--- kotlin/Leaderboard/src/test/kotlin/tddmicroexercises/leaderboard/RaceTest.kt
+++ kotlin/Leaderboard/src/test/kotlin/tddmicroexercises/leaderboard/RaceTest.kt
@@ -20,6 +20,13 @@ class RaceTest {
         assertEquals(0, race.getPoints(driver4))
     }
 
+    @Test
+    fun isShouldNotFailWhenDriverWasNotInRace() {
+        val unknownDriver = Driver("Jordan", "NZ")
+        val race = Race("Australian Grand Prix", driver1, driver2, driver3)
+        assertEquals(0, race.getPoints(unknownDriver))
+    }
+
     private val driver1 = Driver("Nico Rosberg", "DE")
     private val driver2 = Driver("Lewis Hamilton", "UK")
     private val driver3 = Driver("Sebastian Vettel", "DE")
