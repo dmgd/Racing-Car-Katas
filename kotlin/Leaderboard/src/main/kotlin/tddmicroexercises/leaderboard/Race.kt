972d651121ff49c1301290387fecd9a7e2c1fe34
jordan <jordan.r.stewart@gmail.com>
2021-02-19 17:08:22 +0000

    .

--- kotlin/Leaderboard/src/main/kotlin/tddmicroexercises/leaderboard/Race.kt
+++ kotlin/Leaderboard/src/main/kotlin/tddmicroexercises/leaderboard/Race.kt
@@ -1,21 +1,13 @@
 package tddmicroexercises.leaderboard
 
 class Race(private val name: String, vararg val drivers: Driver) {
-    private val driverNames: MutableMap<Driver, String>
 
-    init {
-        this.driverNames = HashMap()
-        for (driver in this.drivers) {
-            this.driverNames[driver] = driver.name
-        }
-    }
-
-    fun position(driver: Driver): Int {
+    private fun position(driver: Driver): Int {
         return this.drivers.indexOf(driver)
     }
 
     fun getPoints(driver: Driver): Int {
-        return Race.POINTS[position(driver)]
+        return POINTS[position(driver)]
     }
 
     fun getDriverName(driver: Driver): String? {
