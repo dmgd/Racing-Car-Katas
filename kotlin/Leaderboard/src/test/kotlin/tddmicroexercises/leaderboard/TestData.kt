c949d24dccddbb24bfba493d6a7b6e0f1f7770f9
jordan <jordan.r.stewart@gmail.com>
2021-02-19 16:47:32 +0000

    .

--- kotlin/Leaderboard/src/test/kotlin/tddmicroexercises/leaderboard/TestData.kt
+++ kotlin/Leaderboard/src/test/kotlin/tddmicroexercises/leaderboard/TestData.kt
@@ -2,22 +2,17 @@ package tddmicroexercises.leaderboard
 
 object TestData {
 
-    var driver1: Driver = Driver("Nico Rosberg", "DE")
-    var driver2: Driver = Driver("Lewis Hamilton", "UK")
-    var driver3: Driver = Driver("Sebastian Vettel", "DE")
-    var driver4: SelfDrivingCar = SelfDrivingCar("1.2", "Acme")
+    val driver1: Driver = Driver("Nico Rosberg", "DE")
+    val driver2: Driver = Driver("Lewis Hamilton", "UK")
+    val driver3: Driver = Driver("Sebastian Vettel", "DE")
+    val driver4: SelfDrivingCar = SelfDrivingCar("1.3", "Acme")
 
-    var race1: Race = Race("Australian Grand Prix", driver1, driver2, driver3)
-    var race2: Race = Race("Malaysian Grand Prix", driver3, driver2, driver1)
-    var race3: Race = Race("Chinese Grand Prix", driver2, driver1, driver3)
-    var race4: Race = Race("Fictional Grand Prix 1", driver1, driver2, driver4)
-    var race5: Race = Race("Fictional Grand Prix 2", driver4, driver2, driver1)
-    var race6: Race = Race("Fictional Grand Prix 3", driver2, driver1, driver4)
+    val race1: Race = Race("Australian Grand Prix", driver1, driver2, driver3)
+    val race2: Race = Race("Malaysian Grand Prix", driver3, driver2, driver1)
+    val race3: Race = Race("Chinese Grand Prix", driver2, driver1, driver3)
+    val race4: Race = Race("Fictional Grand Prix 1", driver1, driver2, driver4)
+    val race5: Race = Race("Fictional Grand Prix 2", driver4, driver2, driver1)
+    val race6: Race = Race("Fictional Grand Prix 3", driver2, driver1, driver4)
 
-    var sampleLeaderboard1: Leaderboard = Leaderboard(race1, race2, race3)
-    var sampleLeaderboard2: Leaderboard = Leaderboard(race4, race5, race6)
-
-    init {
-         driver4.algorithmVersion = "1.3"
-    }
+    val sampleLeaderboard1: Leaderboard = Leaderboard(race1, race2, race3)
 }
